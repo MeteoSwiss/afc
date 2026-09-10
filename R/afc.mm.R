cbdd6cb9eb832fd03e7a119bbd97b139e90ce4f7
bhj <jonas.bhend@meteoswiss.ch>
2026-09-10 08:39:45 +0200

    remove inline comments

--- R/afc.mm.R
+++ R/afc.mm.R
@@ -1,12 +1,12 @@
 #' 2AFC For Ordinal Polychotomous Observations And Ordinal Polychotomous
 #' Forecasts
-#' 
+#'
 #' Routine to calculate the Generalized Discrimination Score (aka
 #' Two-Alternatives Forced Choice Score 2AFC) for the situation of
 #' polychotomous observations (ordinal) and polychotomous forecasts (ordinal)
-#' 
+#'
 #' This routine applies Eq.14 of Mason and Weigel (2009) to calculate the 2AFC.
-#' 
+#'
 #' @param obsv vector with polychotomous observations (values in {1,..,mv})
 #' @param fcst vector of same length as \emph{obsv} with polychotomous
 #' forecasts (values in {1,..,mf})
@@ -20,47 +20,17 @@
 #' framework for administrative purposes. Mon. Wea. Rev., 137, 331-349
 #' @keywords file
 #' @examples
-#' 
-#'   #Forecasts and observations of Nino-3.4 index
-#'   #Load set of polychotomous observations (4 categories) and polychotomous forecasts (4 categories)
 #'   data(cnrm.nino34.mm)
 #'   obsv = cnrm.nino34.mm$obsv
 #'   fcst = cnrm.nino34.mm$fcst
-#' 
-#'   #Calculate skill score
 #'   afc.mm(obsv,fcst,4,4)
-#' 
 #' @export afc.mm
 afc.mm = function(obsv,fcst,mv=3,mf=3){
-
-  #################################
-  # OBSV: POLYCHOTOMOUS (ORDINAL) #
-  # FCST: POLYCHOTOMOUS (ORDINAL) #
-  #################################
-
-  # input variables:
-  # ----------------
-  # fcst - vector with forecast categories (1...mf)
-  # obsv - vector with observation categories (1...mv)
-  # mv   - Number of observation categories (default = 3)
-  # mf   - Number of forecast categories (default = mv)
-
-  # output variable:
-  # ----------------
-  # p.afc - 2AFC skill score as obtained from MW09 Eq. 14
-
-
-  # set mv = mf if mv is not given
   if (mf == 0) mf=mv
-
-  # Matrix with element[nn,mm] being the number of times
-  # that nn has been observed and mm has been forecast
   n.matrix = array(0,dim=c(mv,mf))
   for (nn in 1:mv) for (mm in 1:mf){
     n.matrix[nn,mm] = sum((obsv == nn) & (fcst == mm))
   }
-
-  # Solve Eq. 14 of MW09
   numer = 0
   denom = 0
   for (k in 1:(mv-1)) for (l in (k+1):mv){
